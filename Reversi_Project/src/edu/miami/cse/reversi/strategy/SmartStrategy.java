6159d2135648d051a6d8e05bcd43c8b9c3d11c33
Ben <moormabt@miamioh.edu>
2017-11-02 14:00:39 -0400

    Initial Checkin

---
+++ Reversi_Project/src/edu/miami/cse/reversi/strategy/SmartStrategy.java
@@ -0,0 +1,15 @@
+package edu.miami.cse.reversi.strategy;
+
+import edu.miami.cse.reversi.Board;
+import edu.miami.cse.reversi.Square;
+import edu.miami.cse.reversi.Strategy;
+
+public class SmartStrategy implements Strategy{
+
+	@Override
+	public Square chooseSquare(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
