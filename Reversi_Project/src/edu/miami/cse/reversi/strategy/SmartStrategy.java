ff4ed6549c0c5e709c71fbdc6264b82c425b6398
Qiudong <Qiudong@192.168.1.13>
2017-11-03 19:14:02 -0400

    add some comments

--- Reversi_Project/src/edu/miami/cse/reversi/strategy/SmartStrategy.java
+++ Reversi_Project/src/edu/miami/cse/reversi/strategy/SmartStrategy.java
@@ -1,17 +1,35 @@
 package edu.miami.cse.reversi.strategy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 import edu.miami.cse.reversi.Board;
 import edu.miami.cse.reversi.Square;
 import edu.miami.cse.reversi.Strategy;
 
-//test1
-
+/**
+ * A simple strategy that just chooses with Smart Strategy with alpha-beta
+ *  from the squares available to the current player.
+ */
 public class SmartStrategy implements Strategy{
 
 	@Override
 	public Square chooseSquare(Board board) {
-		// TODO Auto-generated method stub
-		return null;
+		return chooseOne(board.getCurrentPossibleSquares());
 	}
 
+	/**
+	   * A simple utility method for selecting a item with alpha-beta algorithm 
+	   * from a set.
+	   * 
+	   * @param itemSet
+	   *          The set of items from which to select.
+	   * @return A 'best' item to maximize the benefit from the set.
+	   */
+	  public static <T> T chooseOne(Set<T> itemSet) {
+	    List<T> itemList = new ArrayList<>(itemSet);
+	    return itemList.get(new Random().nextInt(itemList.size()));
+	  }
+	
 }
